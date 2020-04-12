df9b7275ab7b15aac9deec9d1b5fa96c68e555b3
Gary W <garywill99@zoho.com>
2020-04-12 19:54:30 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -10,43 +10,34 @@ async function showInfo(details) {
         if (secInfo.state == "insecure") return;
         
         //console.log( secInfo );
-  
-        var root_ca;
-        root_ca = secInfo.certificates[secInfo.certificates.length-1];
-        var root_issuer = "";
-        root_issuer = root_ca.issuer;
-        var root_issuer_array = [];
-        root_issuer_array = root_issuer.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
         
-        var root_issuer_object = {};
-        root_issuer_array.forEach(function(ele) {
-            var eq = ele.indexOf('=');
-            root_issuer_object[ele.substring(0,eq).toUpperCase()] = ele.substring(eq+1);
-        });
-        //console.log(root_issuer_object);
+        const root_ca = secInfo.certificates[secInfo.certificates.length-1];
         
         var s2d = "";
         if (!root_ca.isBuiltInRoot) s2d += "NOT BUILD-IN CA !\n";
         s2d += `${secInfo.state} ${secInfo.protocolVersion}\n`;
-        s2d += 
-            `(${root_issuer_object['C']?root_issuer_object['C']:(
-                root_issuer_object['INCORPORATIONCOUNTRY']?root_issuer_object['INCORPORATIONCOUNTRY']:'?'
-            )})  `;
-        s2d += 
-            root_issuer_object['O']?root_issuer_object['O']:(
-                root_issuer_object['CN']?root_issuer_object['CN']:(
-                    root_issuer_object['OU']?root_issuer_object['OU']:'?'
-                )
-            );
-        
-        /*
+  
+        //console.log(s2d);
+      
+     
+        var infos2show = [];
         for (var i=secInfo.certificates.length-1;i>=0;i--)
         {
-            //s2d += `${secInfo.certificates[i].isBuiltInRoot} issuer: ${secInfo.certificates[i].issuer} \n`;
-            s2d += `${secInfo.certificates[i].subject}\n`
+            //console.log(i);
+            if (i==secInfo.certificates.length-1) // root ca
+            {
+                infos2show.push(showInfoFromParsedCertObj(parseCertStr(secInfo.certificates[i].issuer)));
+                infos2show.push(showInfoFromParsedCertObj(parseCertStr(secInfo.certificates[i].subject)));
+            }else  // not root ca 
+            {
+                infos2show.push(showInfoFromParsedCertObj(parseCertStr(secInfo.certificates[i].subject)));
+            }
+            
         }
+        infos2show = removeNearRepeated(infos2show);
+        s2d += infos2show.join('\n');
         //console.log(s2d );
-        */
+        
         
         browser.notifications.create({
             "type": "basic",
@@ -58,6 +49,39 @@ async function showInfo(details) {
     }
     
 }
+
+function parseCertStr(cert) // input should be a 'issuer' or 'subject' string
+{
+    var comma_array = cert.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/); //find comma and split string
+
+    var shortname_object = {};
+    comma_array.forEach(function(ele) {
+        var eq = ele.indexOf('=');
+        shortname_object[ele.substring(0,eq).toUpperCase()] = ele.substring(eq+1);
+    });
+    
+    return shortname_object;
+}
+
+function showInfoFromParsedCertObj(cert) //input should be object indexed by short names
+{
+    var info = "";
+    //country
+    info += 
+        `(${cert['C']?cert['C']:(
+            cert['INCORPORATIONCOUNTRY']?cert['INCORPORATIONCOUNTRY']:'?'
+        )})  `;
+    // organization or name
+    info += 
+        cert['O']?cert['O']:(
+            cert['CN']?cert['CN']:(
+                cert['OU']?cert['OU']:'?'
+            )
+        );
+
+    return info;
+}
+
 function getUrlHost(s)
 {
     try{
@@ -69,4 +93,18 @@ function getUrlHost(s)
     }catch(e){
         return s;
     }
+}
+
+function removeNearRepeated(arr)
+{
+    for (var i=1;i<arr.length;)
+    {
+        if (arr[i] == arr[i-1])
+        {
+            arr.splice(i,1);
+            continue;
+        }
+        i++;
+    }
+    return arr;
 }
